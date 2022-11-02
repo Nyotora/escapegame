bf0450c84d12b241b8295a2595db11ddca2941dd
Anthony Denise <anthony.denise@etu.unilim.fr>
2022-11-02 22:33:01 +0100

    Initial commit + rdc + 5 rangées de tables et chaises R46/47

---
+++ Assets/Script/scr_Model.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class scr_Model
+{
+    #region - Player -
+
+    [Serializable]
+
+    public class PlayerSettingsModel
+    {
+        [Header("View Settings")]
+        public float ViewXSensitivity;
+        public float ViewYSensitivity;
+
+        public bool ViewXInverted;
+        public bool ViewYInverted;
+
+
+        [Header("Movement")]
+        public float WalkingForwardSpeed;
+        public float WalkingBackwardSpeed;
+        public float WalkingStrafeSpeed;
+    }
+
+
+
+    #endregion
+}
