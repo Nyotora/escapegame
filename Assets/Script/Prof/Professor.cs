ddbdb2f5f1351b013e9fe2f3a9bd03dadf9d2bb8
Anthony Denise <anthony.denise@etu.unilim.fr>
2022-11-23 10:16:20 +0100

    ajout classe Professor + lancement bulle dialogue via interaction

---
+++ Assets/Script/Prof/Professor.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Professor : MonoBehaviour, IInteractable
+{
+    [SerializeField] private string _prompt;
+    public string InteractionPrompt => _prompt;
+    public string[] textLines;
+
+    public Dialogue dialogueBox;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hover()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void Interact(Player player)
+    {
+        dialogueBox.textComponent.text = string.Empty;
+        dialogueBox.lines = textLines;
+        dialogueBox.StartDialogue();
+        Debug.Log("Dialogue ok");
+    }
+
+    public void Unhover()
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    public void VisualInteraction(Player player)
+    {
+        //throw new System.NotImplementedException();
+    }
+}
